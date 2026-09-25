8536b6605efc3866ac57adab512ae81778a51dac
MikhailDimitriev <mishadimit@gmail.com>
2026-09-25 22:20:54 +0300

    Changed images sources

--- src/features/category-news-section-header/model/utils/imageDefinitionByCategory.ts
+++ src/features/category-news-section-header/model/utils/imageDefinitionByCategory.ts
@@ -3,14 +3,14 @@ import type {Categories} from "~/entities/article";
 export function imageDefinitionByCategory(category: Categories) {
   switch (category) {
     case "ai":
-      return 'src/shared/assets/icons/brain.png'
+      return '/src/shared/assets/icons/brain.png'
     case "business":
-      return 'src/shared/assets/icons/business.png'
+      return '/src/shared/assets/icons/business.png'
     case "culture":
-      return 'src/shared/assets/icons/culture.png'
+      return '/src/shared/assets/icons/culture.png'
     case "entertainment":
-      return 'src/shared/assets/icons/entertainment.png'
+      return '/src/shared/assets/icons/entertainment.png'
     case "health":
-      return 'src/shared/assets/icons/health.png'
+      return '/src/shared/assets/icons/health.png'
   }
 }
